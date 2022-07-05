c25916ad5294755aa93c006fd108b037074445d5
Kevin-Wettaka <kevinwettaka@gmail.com>
2022-07-05 08:36:04 -0700

    9. Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+i#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Prints if number is positive, zero or negative
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
