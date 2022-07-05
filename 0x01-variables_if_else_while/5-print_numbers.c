81a6468b9f745ae80add669450dbb6ecc7a1ca0e
Kevin-Wettaka <kevinwettaka@gmail.com>
2022-07-04 20:01:13 -0700

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Prints if number is positive, zero or negative
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		printf("\n");
+	printf("\n");
+	return (0);
+}
